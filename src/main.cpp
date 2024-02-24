8d1a0a89137b5eb61276bcca0cc6e342c59955f7
mkadioglupc <mkadioglupc@mail.com>
2024-02-24 07:32:27 +0300

    remove: RCSwitch kütüphanesi dosyaları ve kodu kaldırıldı. pio sürümü zaten çalışmıyordu eskiydi kütüphane hata veriyordu

--- src/main.cpp
+++ src/main.cpp
@@ -3,163 +3,83 @@
 
 const char msg[] = "LED ON";
 
-#define LED_ON_CODE 123456
-
 #ifdef TX_MODE
 
-// #include <Arduino.h>
-// #include <ESP8266WiFi.h>
-// #include <RH_ASK.h>
-// #include <SPI.h>
-
-// #define PIR_PIN D5
-// #define TRANSMITTER_PIN D6
-
-// // parametreler RH_ASK.h dosyasindan alinabilir
-// RH_ASK driverTx(2000, TRANSMITTER_PIN, D3, D4, true);
-
-// void setup() {
-//   Serial.begin(115200);
-//   pinMode(PIR_PIN, INPUT);
-
-//   // init fonkundaki pin baslatmalari IPTAL EDILMELI
-//   if (!driverTx.init()) {
-//     Serial.println("RF driver initialization failed");
-//   }
-//   driverTx.setModeTx();
-// }
-
-// void loop() {
-//   int motionDetected = digitalRead(PIR_PIN);
-//   Serial.println(motionDetected);
-//   if (motionDetected == HIGH) {
-//     driverTx.send((uint8_t *)msg, strlen(msg));
-//     driverTx.waitPacketSent();
-//   }
-//   delay(250);
-// }
-
-
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include "RCSwitch.h"
+#include <RH_ASK.h>
+#include <SPI.h>
 
 #define PIR_PIN D5
 #define TRANSMITTER_PIN D6
 
-RCSwitch mySwitch = RCSwitch();
+// parametreler RH_ASK.h dosyasindan alinabilir
+RH_ASK driverTx(2000, TRANSMITTER_PIN, D3, D4, true);
 
 void setup() {
   Serial.begin(115200);
-
   pinMode(PIR_PIN, INPUT);
 
-  // Initialize the RCSwitch library with the transmitter pin
-  mySwitch.enableTransmit(TRANSMITTER_PIN);
+  // init fonkundaki pin baslatmalari IPTAL EDILMELI
+  if (!driverTx.init()) {
+    Serial.println("RF driver initialization failed");
+  }
+  driverTx.setModeTx();
 }
 
 void loop() {
   int motionDetected = digitalRead(PIR_PIN);
   Serial.println(motionDetected);
-
   if (motionDetected == HIGH) {
-    mySwitch.send(LED_ON_CODE, 24); // 24 is the number of bits in the code
+    driverTx.send((uint8_t *)msg, strlen(msg));
+    driverTx.waitPacketSent();
   }
-  
-  delay(500);
+  delay(250);
 }
-
-
-
 
 #elif RX_MODE
 
-// #include <Arduino.h>
-// #include <ESP8266WiFi.h>
-// #include <RH_ASK.h>
-// #include <SPI.h>
-
-// #define RECEIVER_PIN D6
-// #define LED_PIN D5
-
-// RH_ASK driverRx(2000, D3, RECEIVER_PIN, D4, false);
-
-// void setup() {
-//   Serial.begin(115200);
-//   pinMode(LED_PIN, OUTPUT);
-
-//   // init fonkundaki pin baslatmalari IPTAL EDILMELI
-//   if (!driverRx.init()) {
-//     Serial.println("RF driver initialization failed");
-//   }
-//   driverRx.setModeRx();
-// }
-
-// void loop() {
-//   //uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
-//   uint8_t buf[10];
-//   uint8_t buflen = sizeof(buf);
-
-//   if (driverRx.recv(buf, &buflen)) {
-//     buf[buflen] = '\0';
-//     Serial.print("Received: ");
-//     Serial.println((char*)buf);
-
-//     digitalWrite(LED_PIN, HIGH);
-//     delay(500);
-//     digitalWrite(LED_PIN, LOW);
-    
-//     // if (strcmp((char*)buf, msg) == 0) {
-//     //   digitalWrite(LED_PIN, HIGH);
-//     //   delay(500);
-//     //   digitalWrite(LED_PIN, LOW);
-//     // }
-//   }
-// }
-
-
-
-
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include "RCSwitch.h"
+#include <RH_ASK.h>
+#include <SPI.h>
 
-// Define the pin connected to the RF receiver
 #define RECEIVER_PIN D6
-
-// Define the pin connected to the LED
 #define LED_PIN D5
 
-RCSwitch mySwitch = RCSwitch();
+RH_ASK driverRx(2000, D3, RECEIVER_PIN, D4, false);
 
 void setup() {
   Serial.begin(115200);
-
-  // Initialize the RCSwitch library with the receiver pin
-  mySwitch.enableReceive(RECEIVER_PIN);
-
-  // Set the LED pin as an output
   pinMode(LED_PIN, OUTPUT);
+
+  // init fonkundaki pin baslatmalari IPTAL EDILMELI
+  if (!driverRx.init()) {
+    Serial.println("RF driver initialization failed");
+  }
+  driverRx.setModeRx();
 }
 
 void loop() {
-  // Check if a signal has been received
-  if (mySwitch.available()) {
-    // Read the received value
-    unsigned long receivedValue = mySwitch.getReceivedValue();
+  //uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+  uint8_t buf[10];
+  uint8_t buflen = sizeof(buf);
 
-    // Check if the received value matches the LED on code
-    if (receivedValue == LED_ON_CODE) {
-      Serial.println("Received LED ON code. Turning LED on.");
+  if (driverRx.recv(buf, &buflen)) {
+    buf[buflen] = '\0';
+    Serial.print("Received: ");
+    Serial.println((char*)buf);
 
-      digitalWrite(LED_PIN, HIGH);
-      delay(500);
-      digitalWrite(LED_PIN, LOW);
-    }
-
-    // Reset the received value for the next transmission
-    mySwitch.resetAvailable();
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    digitalWrite(LED_PIN, LOW);
+    
+    // if (strcmp((char*)buf, msg) == 0) {
+    //   digitalWrite(LED_PIN, HIGH);
+    //   delay(500);
+    //   digitalWrite(LED_PIN, LOW);
+    // }
   }
 }
-
 
 #endif
