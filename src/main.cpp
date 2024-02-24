ffdffd3de61c4e47a60cf00e264e3e55240d74e2
mkadioglupc <mkadioglupc@mail.com>
2024-02-24 13:43:42 +0300

    FEAT: ELHAMDULİLLAH constructor pinleri ters yazıldığı için çalışmıyordu. arduino için kod checkpoint

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
- #define TX_MODE 1
-// #define RX_MODE 2
+// #define TX_MODE 1
+ #define RX_MODE 2
 
 const char msg[] = "LED ON";
 
@@ -14,13 +14,12 @@ const char msg[] = "LED ON";
 #define TRANSMITTER_PIN D6
 
 // parametreler RH_ASK.h dosyasindan alinabilir
-RH_ASK driverTx(2000, TRANSMITTER_PIN, D3, D4, true);
+RH_ASK driverTx(2000, D3, TRANSMITTER_PIN, D4, true);
 
 void setup() {
   Serial.begin(115200);
   pinMode(PIR_PIN, INPUT);
 
-  // init fonkundaki pin baslatmalari IPTAL EDILMELI
   if (!driverTx.init()) {
     Serial.println("RF driver initialization failed");
   }
@@ -30,7 +29,7 @@ void setup() {
 void loop() {
   int motionDetected = digitalRead(PIR_PIN);
   Serial.println(motionDetected);
-  if (motionDetected == HIGH) {
+  if (motionDetected == 1) {
     driverTx.send((uint8_t *)msg, strlen(msg));
     driverTx.waitPacketSent();
   }
@@ -40,20 +39,20 @@ void loop() {
 #elif RX_MODE
 
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
+//#include <ESP8266WiFi.h>
 #include <RH_ASK.h>
 #include <SPI.h>
 
-#define RECEIVER_PIN D6
-#define LED_PIN D5
+#define RECEIVER_PIN 3
+#define LED_PIN 2
 
-RH_ASK driverRx(2000, D3, RECEIVER_PIN, D4, false);
+RH_ASK driverRx(2000, RECEIVER_PIN, 4, 5, false);
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
 
-  // init fonkundaki pin baslatmalari IPTAL EDILMELI
+  // sadece pinleri baslatiyor. rf takili olmasa bile calisiyor
   if (!driverRx.init()) {
     Serial.println("RF driver initialization failed");
   }
@@ -66,19 +65,13 @@ void loop() {
   uint8_t buflen = sizeof(buf);
 
   if (driverRx.recv(buf, &buflen)) {
-    buf[buflen] = '\0';
-    Serial.print("Received: ");
-    Serial.println((char*)buf);
+    //buf[buflen] = '\0';
+    //Serial.print("Received: ");
+    //Serial.println((char*)buf);
 
     digitalWrite(LED_PIN, HIGH);
     delay(500);
     digitalWrite(LED_PIN, LOW);
-    
-    // if (strcmp((char*)buf, msg) == 0) {
-    //   digitalWrite(LED_PIN, HIGH);
-    //   delay(500);
-    //   digitalWrite(LED_PIN, LOW);
-    // }
   }
 }
 
